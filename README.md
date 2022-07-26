883175d3924f06584567d2b4c484b7772e76bda8
nice2cu1 <nice2cu1@outlook.com>
2022-07-26 17:00:30 +0800

    修改推送方式

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # lixianla
 lixianla.com auto sign task
+
+
+# 自动化任务
+
+推荐部署在云函数。
+
+# Github Action
+请clone此项目，自行push到private项目，以免泄露数据，如果你头铁，自行fork并填入相应数据。
+
+
+使用Github Action自动化任务也许存在[任务延迟](https://zhuanlan.zhihu.com/p/379365305)
+
+根据测试，Github Acion任务将整个运行环境部署完成需要花费30s左右，如果有在0点准时打卡的需求，请优先考虑云函数、软路由部署。
+
+在Acion中建议为main函数执行30s左右延迟，并将crontab提前为每天23:59运行（注意时区问题，北京时间比Github所使用时区快8个小时），理论上可以解决此问题，未经测试，请自行试验。
+
+# workflows
+```yaml
+name: auto_task
+
+on:
+  schedule:
+    - cron: "0 16 * * *"
+
+  workflow_dispatch:
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v2
+      - name: Set up Python 3.9
+        uses: actions/setup-python@v2
+        with:
+                python-version: 3.9
+                
+                
+      - name: Install dependencies
+        run: |
+              python -m pip install --upgrade pip
+              pip install requests
+              pip install ddddocr
+              pip install bs4
+      
+      - name: Sign Task
+        run: |
+              python main.py
+```
+
+# 注意事项
+由于[不可抗拒的力量](https://work.weixin.qq.com/nl/act/p/32d807ad4c554975)，企业微信推送方式弃用，切换为钉钉机器人。如有需要请自行查看commits
+
+如果你的应用于2022年6月20日之前创建，或者拥有一个固定的公网非服务商IP，可以使用企业微信进行（更加美观的）图文推送。
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import code
-import random
-import time
-import requests
-from bs4 import BeautifulSoup
-import json
-import ddddocr
 import re
+import hmac
+import time
+import json
+import random
+import base64
+import hashlib
+import requests
+import ddddocr
+import urllib.parse
+from bs4 import BeautifulSoup
 
 ## Contab 
 # 
@@ -26,92 +29,37 @@ import re
 #         soup = BeautifulSoup(response.text,'html.parser')
 #         wecom_app(soup.h4.text.strip(),soup.h4.text.strip())
 
-def wecom_app(title: str, content: str):
+DD_BOT_SECRET = '' #机器人创建时勾选 加签 获取SECRET
+DD_BOT_TOKEN =  '' #机器人Webhook中的access_token部分
+
+def dingding_bot(title: str, content: str) -> None:
     """
-    通过 企业微信 APP 推送消息。
+    使用 钉钉机器人 推送消息。
     """
+    if not DD_BOT_SECRET or not DD_BOT_TOKEN:
+        print("钉钉机器人 服务的 DD_BOT_SECRET 或者 DD_BOT_TOKEN 未设置!!\n取消推送")
+        return
+    print("钉钉机器人 服务启动")
 
-    corpid = '' # 企业id
-    corpsecret = '' # 应用凭证密钥
-    touser = '@all' # 消息推送范围，@all代表整个部门
-    agentid = '' # 应用id
-    try:
-        media_id = '' # 素材库图片media_id
-    except IndexError:
-        media_id = ""
-    wx = WeCom(corpid, corpsecret, agentid)
-    if not media_id:
-        message = title + "\n\n" + content
-        response = wx.send_text(message, touser)
+    timestamp = str(round(time.time() * 1000))
+    secret_enc = DD_BOT_SECRET.encode("utf-8")
+    string_to_sign = "{}\n{}".format(timestamp, DD_BOT_SECRET)
+    string_to_sign_enc = string_to_sign.encode("utf-8")
+    hmac_code = hmac.new(
+        secret_enc, string_to_sign_enc, digestmod=hashlib.sha256
+    ).digest()
+    sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+    url = f'https://oapi.dingtalk.com/robot/send?access_token={DD_BOT_TOKEN}&timestamp={timestamp}&sign={sign}'
+    headers = {"Content-Type": "application/json;charset=utf-8"}
+    data = {"msgtype": "text", "text": {"content": f"{title}\n{content}"}}
+    response = requests.post(
+        url=url, data=json.dumps(data), headers=headers, timeout=15
+    ).json()
+
+    if not response["errcode"]:
+        print("钉钉机器人 推送成功！")
     else:
-        response = wx.send_mpnews(title, content, media_id, touser)
-
-    if response == "ok":
-        print("企业微信推送成功！")
-    else:
-        print("企业微信推送失败！错误信息如下：\n", response)
-
-
-class WeCom:
-    def __init__(self, corpid, corpsecret, agentid):
-        self.CORPID = corpid
-        self.CORPSECRET = corpsecret
-        self.AGENTID = agentid
-
-    def get_access_token(self):
-        url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
-        values = {
-            "corpid": self.CORPID,
-            "corpsecret": self.CORPSECRET,
-        }
-        req = requests.post(url, params=values)
-        data = json.loads(req.text)
-        return data["access_token"]
-
-    def send_text(self, message, touser="@all"):
-        send_url = (
-            "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="
-            + self.get_access_token()
-        )
-        send_values = {
-            "touser": touser,
-            "msgtype": "text",
-            "agentid": self.AGENTID,
-            "text": {"content": message},
-            "safe": "0",
-        }
-        send_msges = bytes(json.dumps(send_values), "utf-8")
-        respone = requests.post(send_url, send_msges)
-        respone = respone.json()
-        return respone["errmsg"]
-
-    def send_mpnews(self, title, message, media_id, touser="@all"):
-        send_url = (
-            "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="
-            + self.get_access_token()
-        )
-        send_values = {
-            "touser": touser,
-            "msgtype": "mpnews",
-            "agentid": self.AGENTID,
-            "mpnews": {
-                "articles": [
-                    {
-                        "title": title,
-                        "thumb_media_id": media_id,
-                        "author": "nice2cu1",
-                        "content_source_url": "",
-                        "content": message.replace("\n", "<br/>"),
-                        "digest": message,
-                    }
-                ]
-            },
-        }
-        send_msges = bytes(json.dumps(send_values), "utf-8")
-        respone = requests.post(send_url, send_msges)
-        respone = respone.json()
-        return respone["errmsg"]
-
+        print("钉钉机器人 推送失败！")
 
 
 
@@ -145,7 +93,7 @@ if __name__=="__main__":
         login_response_bs = BeautifulSoup(login_response.content, 'html.parser')
         if login_response_bs.find(id='body').text.strip() == '密码错误':
             print("登录出错："+login_response_bs.find(id='body').text.strip())
-            wecom_app('密码错误','请检查密码是否为32位小写md5加密！')   
+            dingding_bot('密码错误','请检查密码是否为32位小写md5加密！')   
             break
         elif login_response_bs.find(id='body').text.strip() == '验证码不正确' :
             print("登录出错："+login_response_bs.find(id='body').text.strip()+",将在1s后重试")
@@ -153,7 +101,7 @@ if __name__=="__main__":
         elif login_response_bs.find(id='body').text.strip() == '登录成功':
             print(login_response_bs.find(id='body').text.strip())
         else :
-            print('else :'+login_response_bs.find(id='body').text.strip())
+            print('登录出错：'+login_response_bs.find(id='body').text.strip())
 
         login_msg = login_response_bs.find(id='body').text.strip()
     else:    
@@ -196,12 +144,12 @@ if __name__=="__main__":
                 time.sleep(1)
             elif sign_response_bs.find(id='body').text.strip() == '今天已经签过啦！':
                 sign_msg = 1
-                wecom_app(sign_response_bs.find(id='body').text.strip(),sign_response_bs.find(id='body').text.strip())
+                dingding_bot(sign_response_bs.find(id='body').text.strip(),sign_response_bs.find(id='body').text.strip())
                 break
             elif '成功签到' in sign_response_bs.find(id='body').text.strip() :
                 sign_msg = 1               
                 print(sign_response_bs.find(id='body').text.strip())  
-                wecom_app('签到成功',sign_response_bs.find(id='body').text.strip())
+                dingding_bot('签到成功',sign_response_bs.find(id='body').text.strip())
                 break
             print (f"count = {sign_response_bs.find(id='body').text.strip()}")
             print(f"msg = {sign_msg}")

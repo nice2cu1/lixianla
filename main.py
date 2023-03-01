008b62e1109fba7c5702aceda1cc87b762e9b5b2
nice2cu1 <nice2cu1@outlook.com>
2023-03-01 15:42:10 +0800

    重写

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import re
 import hmac
 import time
 import json
-import random
 import base64
 import hashlib
 import requests
@@ -10,27 +9,10 @@ import ddddocr
 import urllib.parse
 from bs4 import BeautifulSoup
 
-## Contab 
-# 
-# 0 0 * * *
-#
-# 每天0点执行
-#
-#  The script encoding is UTF-8, if you cannot see Chinese comments, please modify the file encoding
-#  The script encoding is UTF-8, if you cannot see Chinese comments, please modify the file encoding
+DD_BOT_SECRET = ''  # 机器人创建时勾选 加签 获取SECRET
+DD_BOT_TOKEN = ''  # 机器人Webhook中的access_token部分
+PUSH_LOG = True  # 是否推送日志
 
-#没有详细分析时编写的代码，无用，仅作记录。
-# def sign():
-#         url = "https://lixianla.com/sg_sign-lx-1059945667.htm"
-#         headers = {
-#         'cookie': '' #cookie
-#         } 
-#         response = requests.request("GET", url, headers=headers)
-#         soup = BeautifulSoup(response.text,'html.parser')
-#         wecom_app(soup.h4.text.strip(),soup.h4.text.strip())
-
-DD_BOT_SECRET = '' #机器人创建时勾选 加签 获取SECRET
-DD_BOT_TOKEN =  '' #机器人Webhook中的access_token部分
 
 def dingding_bot(title: str, content: str) -> None:
     """
@@ -61,106 +43,138 @@ def dingding_bot(title: str, content: str) -> None:
     else:
         print("钉钉机器人 推送失败！")
 
+
 def task(email: str, password: str):
-    login_msg = ''
-    while login_msg != '登录成功':
+    global push_msg
+    login_attempts = 0
+    print_log(f"################### {email} ###################")
+    while login_attempts < 5:
+        login_attempts += 1
+        print_log(f"第{login_attempts}次尝试登录")
         login_session = requests.session()
         login_url = "https://lixianla.com/user-login.htm"
         login_response = login_session.get(login_url)
-        login_bs = BeautifulSoup(login_response.text,'html.parser')
+        login_bs = BeautifulSoup(login_response.text, 'html.parser')
         login_bs.prettify()
-        #登录验证码识别
+        # 登录验证码识别
         login_image_code = login_bs.select('.vcode')
+
         for image in login_image_code:
             img = image['src']
-            img_code_url=f'https://lixianla.com/{img}'
+            img_code_url = f'https://lixianla.com/{img}'
+            print_log("登录验证码地址：" + img_code_url)
             code_response = login_session.get(img_code_url)
             ocr = ddddocr.DdddOcr(show_ad=False)
-            code = ocr.classification(code_response.content)
-            print("登录验证码："+code)
-        #登录数据
+            login_code = ocr.classification(code_response.content)
+            print_log("登录验证码：" + login_code)
+        # 登录
         login_data = {
-            'email': email, #邮箱
-            'password': password, #密码  32位小写md5
-            "vcode": code #验证码
+            'email': email,
+            'password': password,
+            'vcode': login_code,
         }
-        login_response = login_session.post(login_url, data= login_data)
+        login_response = login_session.post(login_url, data=login_data)
         login_response_bs = BeautifulSoup(login_response.content, 'html.parser')
         if login_response_bs.find(id='body').text.strip() == '密码错误':
-            print("登录出错："+login_response_bs.find(id='body').text.strip())
-            dingding_bot('密码错误','请检查密码是否为32位小写md5加密！')   
+            print_log("密码错误")
+            push_msg += f"{email} 密码错误\n"
+            break
+        elif login_response_bs.find(id='body').text.strip() == '加密后长度有问题':
+            print_log("加密后长度有问题")
+            push_msg += f"{email} 加密后长度有问题\n"
             break
         elif login_response_bs.find(id='body').text.strip() == '邮箱不存在':
-            print("登录出错："+login_response_bs.find(id='body').text.strip())
-            dingding_bot('邮箱不存在，请验证邮箱是否有误！')
+            print_log("邮箱不存在，请验证邮箱是否有误！")
             break
-        elif login_response_bs.find(id='body').text.strip() == '验证码不正确' :
-            print("登录出错："+login_response_bs.find(id='body').text.strip()+",将在1s后重试")
-            time.sleep(1)
+        elif login_response_bs.find(id='body').text.strip() == '验证码不正确':
+            print_log("验证码不正确，将在3秒后重试")
+            time.sleep(3)
+        elif login_response_bs.find(id='body').text.strip() == '密码为空':
+            print_log("密码为空")
+            break
         elif login_response_bs.find(id='body').text.strip() == '登录成功':
-            print(login_response_bs.find(id='body').text.strip())
-        else :
-            print('登录出错：'+login_response_bs.find(id='body').text.strip())
-            dingding_bot('登录出错',login_response_bs.find(id='body').text.strip())
+            print_log("登录成功！\n开始执行签到")
+            # 签到
+            sign_attempts = 0
+            while sign_attempts < 3:
+                sign_attempts += 1
+                sign_code_url = "https://lixianla.com/sg_sign.htm"
+                sign_code_response = login_session.get(sign_code_url)
+                sign_code_bs = BeautifulSoup(sign_code_response.content, 'html.parser')
+
+                sign_code_script = sign_code_bs.findAll('script')  # 寻找所有script标签
+                url_temp = "".join(re.findall("'s.*m'", str(sign_code_script))).replace("'", '')  # list转string、正则、去' 处理
+
+                real_sign_url = f'https://lixianla.com/{url_temp}'
+                sign_response = login_session.get(real_sign_url)
+                sign_bs = BeautifulSoup(sign_response.content, 'html.parser')
+                sign_bs.prettify()
+
+                # 签到验证码识别
+                sign_code = ''
+                sign_img_code = sign_bs.select('.vcode')
+                for image in sign_img_code:
+                    img = image['src']
+                    img_code_url = f'https://lixianla.com/{img}'
+                    print_log("签到验证码地址：" + img_code_url)
+                    code_response = login_session.get(img_code_url)
+                    sign_code = ocr.classification(code_response.content)
+                    print_log("签到验证码：" + sign_code)
+
+                sign_data = {
+                    'vcode': sign_code
+                }
+                time.sleep(1)
+                sign_response = login_session.post(real_sign_url, data=sign_data)
+                sign_response_bs = BeautifulSoup(sign_response.content, 'html.parser')
+                print_log(
+                    f"sign_response_bs.find(id='body').text.strip() ======== {sign_response_bs.find(id='body').text.strip()}")
+                if sign_response_bs.find(id='body').text.strip() == '验证码错误!!!':
+                    print_log("签到出错：" + sign_response_bs.find(id='body').text.strip() + ",将在1s后重试")
+                    push_msg += f"{email}   签到出错：" + sign_response_bs.find(id='body').text.strip() + "\n"
+                    time.sleep(1)
+                elif sign_response_bs.find(id='body').text.strip() == "今天已经签过啦！":
+                    print_log("今天已经签过啦！")
+                    push_msg += f"{email} 今天已经签过啦！\n"
+                    break
+                elif '成功签到' in sign_response_bs.find(id='body').text.strip():
+                    print_log(sign_response_bs.find(id='body').text.strip())
+                    push_msg += f"{email}：{sign_response_bs.find(id='body').text.strip()}\n"
+                    break
+            break
+        else:
+            print_log("未知错误：" + login_response_bs.find(id='body').text.strip())
+            push_msg += f"{email} 出现未被捕获的错误\n"
+            time.sleep(1)
             break
 
-        login_msg = login_response_bs.find(id='body').text.strip()
-    else:    
-        #签到
-        print('开始执行签到....')
-        sign_msg = 0
-        while sign_msg != 1:     
-            sign_code_url = 'https://lixianla.com/sg_sign.htm'
-            sign_code_response = login_session.get(sign_code_url)
-            sign_code_bs = BeautifulSoup(sign_code_response.content,'html.parser')
 
-            sign_code_script = sign_code_bs.findAll('script') #寻找所有script标签
-            url_temp = "".join(re.findall("'s.*m'", str(sign_code_script))).replace("'",'') #list转string、正则、去' 处理
+def print_log(log: str):
+    with open("log.txt", 'a', encoding="UTF-8") as file:
+        file.write(log + "\n")
+        print(log)
 
-            real_sign_url = f'https://lixianla.com/{url_temp}'
-            cookie = login_response.cookies
-            sign_response = login_session.get(real_sign_url)
-            sign_bs = BeautifulSoup(sign_response.content,'html.parser')
-            sign_bs.prettify()
 
-            sign_code = ''
-            #签到验证码识别
-            sign_img_code = sign_bs.select('.vcode')
-            for image in sign_img_code:
-                img_code_url=f'https://lixianla.com/{img}'
-                code_response = login_session.get(img_code_url)
-                sign_code = ocr.classification(code_response.content)
-                print("签到验证码："+sign_code)
+if __name__ == '__main__':
+    push_msg = ""
+    # 创建log文件
+    with open("log.txt", 'w', encoding="UTF-8") as f:
+        f.writelines("####日志文件####\n")
 
-            sign_data = {
-                'vcode' : sign_code
-            }
-                
-            sign_response = login_session.post(real_sign_url,data=sign_data)
-            sign_response_bs = BeautifulSoup(sign_response.content, 'html.parser')
-            print(f"sign_response_bs.find(id='body').text.strip() ======== {sign_response_bs.find(id='body').text.strip()}")
-            if sign_response_bs.find(id='body').text.strip() == '验证码错误!!!':
-                sign_msg = 0               
-                print("签到出错："+sign_response_bs.find(id='body').text.strip()+",将在1s后重试")
-                time.sleep(1)
-            elif sign_response_bs.find(id='body').text.strip() == '今天已经签过啦！':
-                sign_msg = 1
-                dingding_bot(sign_response_bs.find(id='body').text.strip(),sign_response_bs.find(id='body').text.strip())
-                break
-            elif '成功签到' in sign_response_bs.find(id='body').text.strip() :
-                sign_msg = 1               
-                print(sign_response_bs.find(id='body').text.strip())  
-                dingding_bot('签到成功',sign_response_bs.find(id='body').text.strip())
-                break
-            print (f"count = {sign_response_bs.find(id='body').text.strip()}")
-            print(f"msg = {sign_msg}")
-            print("-----------------------------------")
-
-if __name__=="__main__":
-    # time.sleep(random.randint(0,5)) #随机延迟
-    # sign()                         
-    with open('user.json', 'r', encoding='utf-8') as f:
-        user = json.load(f)
-    for i in user:
-        task(i['email'],i['password'])
-        time.sleep(random.randint(0,5))
+    # 读取配置文件
+    try:
+        with open("user.json", 'r') as f:
+            user = json.load(f)
+            for u in user:
+                if u['email'] == "":
+                    print_log("邮箱为空，跳过")
+                    continue
+                task(u['email'], u['password'])
+            if PUSH_LOG:
+                with open("log.txt", 'r', encoding="UTF-8") as f:
+                    push_msg += f.read()
+            dingding_bot(" LIXIANLA ", push_msg)
+    except FileNotFoundError as e:
+        print_log("配置文件不存在！！\n" + str(e))
+    except json.decoder.JSONDecodeError as e:
+        print_log("JSON格式错误！！\n" + str(e))
